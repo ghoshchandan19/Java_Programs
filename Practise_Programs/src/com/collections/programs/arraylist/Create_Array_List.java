f3dc2c7e40106c36f38144b82b7b96820281a0fb
chandan ghosh <ghoshchandan19@gmail.com>
2018-08-26 22:50:15 +0530

    Programs from collections

---
+++ Practise_Programs/src/com/collections/programs/arraylist/Create_Array_List.java
@@ -0,0 +1,18 @@
+package com.collections.programs.arraylist;
+
+import java.util.ArrayList;
+
+public class Create_Array_List {
+
+	public static void main(String[] args) {
+		ArrayList<String> list_Strings = new ArrayList<String>();
+		//To add into the list we use add method
+		  list_Strings.add("Red");
+		  list_Strings.add("Green");
+		  list_Strings.add("Orange");
+		  list_Strings.add("White");
+		  list_Strings.add("Black");
+		  System.out.println(list_Strings);
+	}
+
+}
