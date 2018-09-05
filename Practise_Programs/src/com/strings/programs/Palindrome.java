1858d3a590c9888a5dcfa2d1d441e82490c7824f
chandan ghosh <ghoshchandan19@gmail.com>
2018-09-06 02:02:25 +0530

    Arranged the structure

--- Practise_Programs/src/com/strings/programs/Palindrome.java
+++ Practise_Programs/src/com/strings/programs/Palindrome.java
@@ -1,16 +1,18 @@
 package com.strings.programs;
 
-public class Palindrome {
+public class Palindrome 
+{
 
-	public static void main(String[] args) {
-		String nm="cooc";
-		 int l =nm.length();//To store length of the string
+	public static void main(String[] args) 
+	{
+		   String nm="cooc";
+		   int l =nm.length();//To store length of the string
 		   String B="";
 	       for(int i=0;i<=l-1;i++)
 	       {
 	    	char d=nm.charAt(i);
 	    	B=d+B;//Used to reverse the string
-	}
+	       }
 	       if(B.equals(nm))//Check reverse String with original String
 	       {
 	    	   System.out.println("String is Palindrome");
