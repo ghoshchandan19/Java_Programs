f2e59eeaa13f4060d84b69580bc29dafcc4ba281
chandan ghosh <ghoshchandan19@gmail.com>
2018-09-11 20:50:28 +0530

    Added few pattern programs

---
+++ Practise_Programs/src/com/condutional_stements/Pattern8.java
@@ -0,0 +1,30 @@
+/*
+1 2 3  
+4 5 6  
+7 8 9  
+10 11 12  
+13 14 15  
+
+ */
+
+
+package com.condutional_stements;
+
+public class Pattern8 {
+
+	public static void main(String[] args) {
+		int k=0;
+		for(int i=1;i<=5;i++)
+		{
+			for(int j=1;j<=3;j++)
+			{
+				k=k+1;
+			
+				System.out.print(k+" ");
+			}
+			System.out.println(" ");
+
+	}
+
+}
+}
